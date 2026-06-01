4e01d148484dc6d704007e57870a3175b5e674b3
ghxstloner <yoinerdeyhan@gmail.com>
2026-06-01 14:13:31 -0500

    📝 [add] v1.0.0

--- amaxoniaerp-pos/app/build.gradle.kts
+++ amaxoniaerp-pos/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 3
-        versionName = "0.9.3"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
