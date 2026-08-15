7aee01505e40e21a2c740adf5e200198c1ba1a94
Yoiner Moreno <yoinerdeyhan@gmail.com>
2026-08-15 02:18:24 -0500

    Remove unused backend wildcard imports

--- amaxoniaerp-backend/src/main/kotlin/Frameworks.kt
+++ amaxoniaerp-backend/src/main/kotlin/Frameworks.kt
@@ -1,25 +1,11 @@
 package com.amaxoniaerp
 
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.application.log
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import org.jetbrains.exposed.sql.*
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
-import org.slf4j.event.*
 
 fun Application.configureFrameworks() {
     install(Koin) {
