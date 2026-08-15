5cf7b3e9eb71ca9ce2e33f037a79072ec3c8d728
Yoiner Moreno <yoinerdeyhan@gmail.com>
2026-08-15 02:18:38 -0500

    Remove unused monitoring wildcard imports

--- amaxoniaerp-backend/src/main/kotlin/Monitoring.kt
+++ amaxoniaerp-backend/src/main/kotlin/Monitoring.kt
@@ -1,21 +1,9 @@
 package com.amaxoniaerp
 
-import io.ktor.http.path
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.Application
-import io.ktor.server.application.call
 import io.ktor.server.application.install
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.plugins.swagger.*
+import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.request.path
-import io.ktor.server.response.*
-import io.ktor.server.routing.path
-import org.jetbrains.exposed.sql.*
 import org.slf4j.event.Level
 
 fun Application.configureMonitoring() {
