0e96a110f2b025b16148c38b6544eb5fe980d884
Yoiner Moreno <yoinerdeyhan@gmail.com>
2026-08-15 21:59:53 -0500

    style: resolve backend declaration naming findings

--- amaxoniaerp-backend/src/main/kotlin/com/amaxoniaerp/HTTP.kt
+++ amaxoniaerp-backend/src/main/kotlin/com/amaxoniaerp/HTTP.kt
@@ -19,6 +19,6 @@ fun Application.configureHTTP() {
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
         allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        anyHost() // Production deployments should restrict allowed hosts when feasible.
     }
 }
