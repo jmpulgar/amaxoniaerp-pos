721550477e6be35be2e81f6fd7997e91ed41a98d
ghxstloner <yoinerdeyhan@gmail.com>
2026-08-23 16:31:38 -0500

    refactor(fase14/task160): elimina leftover Ktor starter - ruta muerta /json/kotlinx-serialization y UsersSchema.kt sin referencias; gates green

--- amaxoniaerp-backend/src/main/kotlin/com/amaxoniaerp/Serialization.kt
+++ amaxoniaerp-backend/src/main/kotlin/com/amaxoniaerp/Serialization.kt
@@ -2,12 +2,8 @@ package com.amaxoniaerp
 
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
-import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.server.response.respond
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
 import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
@@ -19,11 +15,5 @@ fun Application.configureSerialization() {
                 explicitNulls = false
             },
         )
-    }
-
-    routing {
-        get("/json/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "world"))
-        }
     }
 }
